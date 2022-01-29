7da40b1959b677fe563f5334457af9971b3fa85f
scori1 <freetiktik@gmail.com>
2022-01-30 03:03:41 +0700

    namarepo

---
+++ srv/zero.js
@@ -0,0 +1,458 @@
+var {
+WAConnection,
+MessageType,
+Presence,
+MessageOptions,
+Mimetype,
+WALocationMessage,
+WA_MESSAGE_STUB_TYPES,
+WA_DEFAULT_EPHEMERAL,
+ReconnectMode,
+ProxyAgent,
+GroupSettingChange,
+waChatKey,
+mentionedJid,
+processTime,
+} = require('@adiwajshing/baileys')
+var { color, bgcolor } = require('../mess/color')
+var speed = require('performance-now')
+var ggs = require('google-it')
+var googleImage = require('g-i-s')
+var { EmojiAPI } = require("emoji-api")
+var emoji = new EmojiAPI()
+var { wait, simih, getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, banner, start, info, success, close } = require('../mess/myfunc')
+var { fetchJson, kyun, fetchText } = require('../mess/fetcher')
+var Exif = require('../mess/exif')
+var exif = new Exif()
+var request = require('request')
+var translate = require('@vitalets/google-translate-api')
+var fs = require('fs')
+var hx = require("hxz-api")
+var cheerio = require('cheerio')
+var brainly = require('brainly-scraper')
+var axios = require("axios")
+var ffmpeg = require('fluent-ffmpeg')
+var fetch = require('node-fetch')
+var crypto = require('crypto')  
+var yts = require('yt-search')
+var Math_js = require('mathjs')
+var { removeBackgroundFromImageFile } = require('remove.bg')
+var moment = require('moment-timezone')
+var { exec, spawn, execSync } = require('child_process')
+faketeks = '© - FucxXx'
+blocked = []
+multi =false
+nopref = false
+allpref = true
+
+// Storage!
+
+var welcome = JSON.parse(fs.readFileSync('./storage/welcome.json'))
+var setting = JSON.parse(fs.readFileSync('./setting.json'))
+var antilink = JSON.parse(fs.readFileSync('./storage/antilink.json'))
+var config = JSON.parse(fs.readFileSync('./srv/config.js'))
+var antivirtex = JSON.parse(fs.readFileSync('./storage/antivirtex.json'))
+var pendaftar = JSON.parse(fs.readFileSync('./storage/user.json'))
+
+// Module Export!
+ 
+module.exports = zero = async (zero, zer) => {
+try {
+if (!zer.hasNewMessage) return
+zer = zer.messages.all()[0]
+if (!zer.message) return
+if (zer.key && !zer.key.remoteJid == 'status@broadcast') return
+global.blocked
+zer.message = (Object.keys(zer.message)[0] === 'ephemeralMessage') ? zer.message.ephemeralMessage.message : zer.message
+var typei = Object.keys(zer.message)[0]
+global.prefix
+var content = JSON.stringify(zer.message)
+var from = zer.key.remoteJid
+var type = Object.keys(zer.message)[0]
+var { text, extendedText, contact, location, liveLocation, image, video, sticker, document, audio } = MessageType
+var time = moment.tz('Asia/Jakarta').format('DD/MM HH:mm:ss')
+var datre = new Date().toLocaleDateString()
+var wib = moment.tz('Asia/Jakarta').format('HH : mm : ss')
+var wita = moment.tz('Asia/Makassar').format('HH : mm : ss')
+var wit = moment.tz('Asia/Jayapura').format('HH : mm :ss')
+
+var dates = moment().tz('Asia/Jakarta').format("YYYY-MM-DDTHH:mm:ss");
+var date = new Date(dates);
+var tahun = date.getFullYear();
+var bulan1 = date.getMonth();
+var tanggal = date.getDate();
+var hari = date.getDay();
+var haris = date.getDay();
+var jam = date.getHours();
+var menit = date.getMinutes();
+var detik = date.getSeconds();
+var waktoo = date.getHours();
+var cmd = (type === 'conversation' && zer.message.conversation) ? zer.message.conversation : (type == 'imageMessage') && zer.message.imageMessage.caption ? zer.message.imageMessage.caption : (type == 'videoMessage') && zer.message.videoMessage.caption ? zer.message.videoMessage.caption : (type == 'extendedTextMessage') && zer.message.extendedTextMessage.text ? zer.message.extendedTextMessage.text : ''.slice(1).trim().split(/ +/).shift().toLowerCase()
+if (multi){
+var prefix = /^[°zZ#$@*+,.?=''():√%!¢£¥€π¤ΠΦ_&><`™©®Δ^βα~¦|/\\©^]/.test(cmd) ? cmd.match(/^[°zZ#$@*+,.?=''():√%¢£¥€π¤ΠΦ_&><!`™©®Δ^βα~¦|/\\©^]/gi) : '.'
+} else {
+if (nopref){
+prefix = ''
+} else {
+if (allpref){
+var prefix = /^[°zZ#$@*+,.?=''():√%!¢£¥€π¤ΠΦ_&><`™©®Δ^βα~¦|/\\©^]/.test(cmd) ? cmd.match(/^[°zZ#$@*+,.?=''():√%¢£¥€π¤ΠΦ_&><!`™©®Δ^βα~¦|/\\©^]/gi) : ''
+} else {
+prefix = prefa
+}
+}
+}
+body = (type === 'conversation' && zer.message.conversation.startsWith(prefix)) ? zer.message.conversation : (type == 'imageMessage') && zer.message[type].caption.startsWith(prefix) ? zer.message[type].caption : (type == 'videoMessage') && zer.message[type].caption.startsWith(prefix) ? zer.message[type].caption : (type == 'extendedTextMessage') && zer.message[type].text.startsWith(prefix) ? zer.message[type].text : (type == 'listResponseMessage') && zer.message[type].singleSelectReply.selectedRowId ? zer.message[type].singleSelectReply.selectedRowId : (type == 'buttonsResponseMessage') && zer.message[type].selectedButtonId ? zer.message[type].selectedButtonId : ''
+var budo = (typei === 'conversation') ? zer.message.conversation : (typei === 'extendedTextMessage') ? zer.message.extendedTextMessage.text : ''
+var budy = (type === 'conversation') ? zer.message.conversation : (type === 'extendedTextMessage') ? zer.message.extendedTextMessage.text : ''
+var command = body.replace(prefix, '').trim().split(/ +/).shift().toLowerCase()
+var args = body.trim().split(/ +/).slice(1)
+var isCmd = body.startsWith(prefix)
+var arg = budy.slice(command.length + 2, budy.length)
+var q = args.join(' ')
+var timestampi = speed();
+var latensyi = speed() - timestampi
+var pes = (type === 'conversation' && zer.message.conversation) ? zer.message.conversation : (type == 'imageMessage') && zer.message.imageMessage.caption ? zer.message.imageMessage.caption : (type == 'videoMessage') && zer.message.videoMessage.caption ? zer.message.videoMessage.caption : (type == 'extendedTextMessage') && zer.message.extendedTextMessage.text ? zer.message.extendedTextMessage.text : ''
+var messagesD = pes.slice(0).trim().split(/ +/).shift().toLowerCase()
+var botNumber = zero.user.jid
+var Verived = "0@s.whatsapp.net"
+var num = "6285755825621@s.whatsapp.net"
+var isGroup = from.endsWith('@g.us')
+var sender = zer.key.fromMe ? zero.user.jid : isGroup ? zer.participant : zer.key.remoteJid
+var senderNumber = sender.split("@")[0] 
+var conts = zer.key.fromMe ? zero.user.jid : zero.contacts[sender] || { notify: jid.replace(/@.+/, '') }
+var pushname = zer.key.fromMe ? zero.user.name : conts.notify || conts.vname || conts.name || '-'
+var groupMetadata = isGroup ? await zero.groupMetadata(from) : ''
+var groupName = isGroup ? groupMetadata.subject : ''
+var groupId = isGroup ? groupMetadata.jid : ''
+var groupMembers = isGroup ? groupMetadata.participants : ''
+var groupAdmins = isGroup ? getGroupAdmins(groupMembers) : ''
+var groupDesc = isGroup ? groupMetadata.desc : ''
+var groupOwner = isGroup ? groupMetadata.owner : ''
+var isUser = pendaftar.includes(sender)
+var isWelcome = isGroup ? welcome.includes(from) : false
+var isGroupAdmins = groupAdmins.includes(sender) || false
+var isBotGroupAdmins = groupAdmins.includes(botNumber) || false
+var isAntilink = isGroup ? antilink.includes(from) : false
+var isAntivirtex = isGroup ? antivirtex.includes(from) : false
+var isButton = (type == 'buttonsResponseMessage') ? zer.message.buttonsResponseMessage.selectedButtonId : ''
+
+// Connection!
+		
+var sendFileFromUrl = async(link, type, options) => {
+hasil = await getBuffer(link)
+zero.sendMessage(from, hasil, type, options).catch(e => {
+fetch(link).then((hasil) => {
+zero.sendMessage(from, hasil, type, options).catch(e => {
+zero.sendMessage(from, { url : link }, type, options).catch(e => {
+reply('*Error Dalam Mengirim Media!*')
+console.log(e)
+})
+})
+})
+})
+}	
+var isUrl = (url) => {
+return url.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%.+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%+.~#?&/=]*)/, 'gi'))
+}
+var reply = (teks) => {
+zero.sendMessage(from, teks, text, {quoted:ftrol})
+}
+var sendMess = (hehe, teks) => {
+zero.sendMessage(hehe, teks, text)
+}
+var mentions = (teks, memberr, id) => {
+(id == null || id == undefined || id == false) ? zero.sendMessage(from, teks.trim(), extendedText, { contextInfo: { "mentionedJid": memberr } }) : zero.sendMessage(from, teks.trim(), extendedText, { quoted: ftrol, contextInfo: { "mentionedJid": memberr } })
+}
+var costum = (pesan, tipe, target, target2) => {
+zero.sendMessage(from, pesan, tipe, { quoted: { key: { fromMe: false, participant: `${target}`, ...(from ? { remoteJid: from } : {}) }, message: { conversation: `${target2}` } } })
+}
+        
+zero.chatRead(from, "read")
+
+// Fake!
+
+var ftrol = {
+key : {
+participant : '0@s.whatsapp.net'
+},
+message: {
+orderMessage: {
+itemCount : 555,
+status: 1,
+surface : 1,
+message: `© - RyGii`, 
+orderTitle: `© - RyGii`,
+sellerJid: '0@s.whatsapp.net' 
+}
+}
+}
+                      
+// Button!
+
+var sendButMessage = (id, text1, desc1, but = [], options = {}) => {
+var buttonMessage = {
+contentText: text1,
+footerText: desc1,
+buttons: but,
+headerType: 1,
+};
+zero.sendMessage(
+id,
+buttonMessage,
+MessageType.buttonsMessage,
+options
+)
+}
+async function sendButLocation(id, text1, desc1, gam1, but = [], options = {}) {
+let buttonMessages = { locationMessage: { jpegThumbnail: gam1 }, contentText: text1, footerText: desc1, buttons: but, headerType: 6 }
+return zero.sendMessage(id, buttonMessages, MessageType.buttonsMessage, options)
+}
+
+// Connection2!
+
+var sendStickerFromUrl = async(to, url) => {
+var names = Date.now() / 10000;
+var download = function (uri, filename, callback) {
+request.head(uri, function (err, res, body) {
+request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+});
+};
+download(url, './stik' + names + '.png', async function () {
+console.log('selesai');
+let filess = './stik' + names + '.png'
+let asw = './stik' + names + '.webp'
+exec(`ffmpeg -i ${filess} -vcodec libwebp -filter:v fps=fps=20 -lossless 1 -loop 0 -preset default -an -vsync 0 -s 512:512 ${asw}`, (err) => {
+let media = fs.readFileSync(asw)
+zero.sendMessage(to, media, MessageType.sticker,{quoted:ftrol})
+fs.unlinkSync(filess)
+fs.unlinkSync(asw)
+});
+});
+}
+var sendMediaURL = async(to, url, text="", mids=[]) =>{
+if(mids.length > 0){
+text = normalizeMention(to, text, mids)
+}
+var fn = Date.now() / 10000;
+var filename = fn.toString()
+let mime = ""
+var download = function (uri, filename, callback) {
+request.head(uri, function (err, res, body) {
+mime = res.headers['content-type']
+request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+});
+};
+download(url, filename, async function () {
+console.log('done');
+let media = fs.readFileSync(filename)
+let type = mime.split("/")[0]+"Message"
+if(mime === "image/gif"){
+type = MessageType.video
+mime = Mimetype.gif
+}
+if(mime.split("/")[0] === "audio"){
+mime = Mimetype.mp4Audio
+}
+zero.sendMessage(to, media, type, { quoted: ftrol, mimetype: mime, caption: text,contextInfo: {"mentionedJid": mids}})
+                    
+fs.unlinkSync(filename)
+});
+}
+
+// Message Group!
+
+if (isGroup && isAntilink && !zer.key.fromMe) {
+if (budy.includes("://chat.whatsapp.com/")) {
+if (isGroupAdmins) return reply("admin bebas");
+reply("ANTILINK DETECTED!! Maaf Kamu Dikick Dari Group");
+zero.groupRemove(from, [sender]);
+}
+}
+if (budy.length > 3500) {
+if (!isGroup) return
+if (!isAntivirtex) return
+if (isGroupAdmins) return
+reply('Tandai telah dibaca\n'.repeat(300))
+reply(`「 VIRTEX DETECTED!! 」\n\nKamu Mengirimkan Virus, Maaf Kamu Di Kick Dari Group :(`)
+console.log(color('[KICK]', 'red'), color('Received a virus text!', 'yellow'))
+zero.groupRemove(from, [sender])
+}   
+
+
+// Media!
+            
+colors = ['red', 'white', 'black', 'blue', 'yellow', 'green']
+var isMedia = (type === 'imageMessage' || type === 'videoMessage')
+var isQuotedImage = type === 'extendedTextMessage' && content.includes('imageMessage')
+var isQuotedVideo = type === 'extendedTextMessage' && content.includes('videoMessage')
+var isQuotedAudio = type === 'extendedTextMessage' && content.includes('audioMessage')
+var isQuotedSticker = type === 'extendedTextMessage' && content.includes('stickerMessage')
+if (!isGroup && isCmd) console.log('\x1b[1;31m~\x1b[1;37m>', '[\x1b[1;32mEXEC\x1b[1;37m]', time, color(command), 'from', color(sender.split('@')[0]), 'args :', color(args.length))
+if (isCmd && isGroup) console.log('\x1b[1;31m~\x1b[1;37m>', '[\x1b[1;32mEXEC\x1b[1;37m]', time, color(command), 'from', color(sender.split('@')[0]), 'in', color(groupName), 'args :', color(args.length))
+
+// Fitur!
+
+switch (command) {                                             
+case 'sticker':
+case 'stiker':
+case 's':
+if ((isMedia && !zer.message.videoMessage || isQuotedImage) && args.length == 0) {
+let encmedia = isQuotedImage ? JSON.parse(JSON.stringify(zer).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : zer
+let media = await zero.downloadAndSaveMediaMessage(encmedia, './storage/media_user')
+ran = getRandom('.webp')
+await ffmpeg(`./${media}`)
+.input(media)
+.on('start', function (cmd) {
+console.log(`Started : ${cmd}`)
+})
+.on('error', function (err) {
+console.log(`Error : ${err}`)
+fs.unlinkSync(media)
+reply(mess.error.stick)
+})
+.on('end', function () {
+console.log('Finish')
+buffer = fs.readFileSync(ran)
+costum(buffer, sticker)
+fs.unlinkSync(media)
+fs.unlinkSync(ran)
+})
+.addOutputOptions([`-vcodec`, `libwebp`, `-vf`, `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+.toFormat('webp')
+.save(ran)
+} else if ((isMedia && zer.message.videoMessage.seconds < 11 || isQuotedVideo && zer.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage.seconds < 11) && args.length == 0) {
+let encmedia = isQuotedVideo ? JSON.parse(JSON.stringify(zer).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : zer
+let media = await zero.downloadAndSaveMediaMessage(encmedia, './storage/media_user')
+ran = getRandom('.webp')
+reply(mess.wait)
+await ffmpeg(`./${media}`)
+.inputFormat(media.split('.')[1])
+.on('start', function (cmd) {
+console.log(`Started : ${cmd}`)
+})
+.on('error', function (err) {
+console.log(`Error : ${err}`)
+fs.unlinkSync(media)
+tipe = media.endsWith('.mp4') ? 'video' : 'gif'
+reply(`*Error. Coba Lagi!*`)
+})
+.on('end', function () {
+console.log('Finish')
+costum(fs.readFileSync(ran), sticker, Verived, `~ Nih Dah Jadi Gif Stikernya`)
+fs.unlinkSync(media)
+fs.unlinkSync(ran)
+})
+.addOutputOptions([`-vcodec`, `libwebp`, `-vf`, `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+.toFormat('webp')
+.save(ran)
+} else if ((isMedia || isQuotedImage) && args[0] == 'nobg') {
+let encmedia = isQuotedImage ? JSON.parse(JSON.stringify(zer).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : zer
+let media = await zero.downloadAndSaveMediaMessage(encmedia, './storage/media_user')
+ranw = getRandom('.webp')
+ranp = getRandom('.png')
+reply(mess.wait)
+keyrmbg = 'bcAvZyjYAjKkp1cmK8ZgQvWH'
+await removeBackgroundFromImageFile({ path: media, apiKey: keyrmbg, size: 'auto', type: 'auto', ranp }).then(res => {
+fs.unlinkSync(media)
+let buffer = Buffer.from(res.base64img, 'base64')
+fs.writeFileSync(ranp, buffer, (err) => {
+if (err) return reply('*Error. Coba Lagi!*')
+})
+exec(`ffmpeg -i ${ranp} -vcodec libwebp -filter:v fps=fps=20 -lossless 1 -loop 0 -preset default -an -vsync 0 -s 512:512 ${ranw}`, (err) => {
+fs.unlinkSync(ranp)
+if (err) return reply(mess.error.stick)
+zero.sendMessage(from, fs.readFileSync(ranw), sticker, { quoted: ftrol })
+fs.unlinkSync(ranw)
+})
+})
+} else {
+reply(`*Kirim Gambar 🖼 Dengan Caption ${prefix}Sticker*`)
+}
+break
+case 'toimg':
+if (!isQuotedSticker) return reply('*Tag Sticker!*')
+encmedia = JSON.parse(JSON.stringify(zer).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo
+media = await zero.downloadAndSaveMediaMessage(encmedia, './storage/media_user')
+ran = getRandom('.png')
+exec(`ffmpeg -i ${media} ${ran}`, (err) => {
+fs.unlinkSync(media)
+if (err) return reply('*Error!*')
+buffer = fs.readFileSync(ran)
+costum(buffer, image)
+fs.unlinkSync(ran)
+})
+break
+case 'kick':
+if (!isGroup) return reply(mess.only.group)
+if (!isGroupAdmins) return reply(mess.only.admin)
+if (!isBotGroupAdmins) return reply(mess.only.bodmin)
+if(!q)return reply(`*Tag Target! 🎯*`)
+if (!isBotGroupAdmins) return reply(mess.only.Badmin)
+y = q.split('@')[1] + '@s.whatsapp.net'
+zero.groupRemove(from, [y])
+reply(`*Succes Kick Target! 🎯*`)
+break
+case 'group':
+case 'grup':
+if (!isGroupAdmins) return reply(mess.only.admin)
+if (!isGroup) return reply(mess.only.group)
+if (!isBotGroupAdmins) return reply(mess.only.bodmin)
+but = [
+{ buttonId: 'groupbuka', buttonText: { displayText: 'Buka 🌝' }, type: 1 },
+{ buttonId: 'grouptutup', buttonText: { displayText: 'Tutup 🌚' }, type: 1 }
+]
+sendButMessage(from, "*Silahkan Admin Pilih!*", faketeks, but, zer)
+break
+case 'groupbuka' :
+if (!isGroup) return reply(mess.only.group)
+if (!isGroupAdmins) return reply(mess.only.admin)
+if (!isBotGroupAdmins) return reply(mess.only.bodmin)
+reply(`\`\`\`Sukses Membuka Group!\`\`\` *${groupMetadata.subject}*`)
+zero.groupSettingChange(from, GroupSettingChange.messageSend, false)
+break
+case 'grouptutup' :
+if (!isGroup) return reply(mess.only.group)
+if (!isGroupAdmins) return reply(mess.only.admin)
+if (!isBotGroupAdmins) return reply(mess.only.bodmin)
+reply(`\`\`\`Sukses Menutup Group!\`\`\` *${groupMetadata.subject}*`)
+zero.groupSettingChange(from, GroupSettingChange.messageSend, true)
+break
+case 'linkgrup' :
+case 'linkgc' :
+if (!isGroup) return reply(mess.only.group)
+if (!isBotGroupAdmins) return reply(mess.only.bodmin)
+linkgc = await zero.groupInviteCode(from)
+yeh = `*https://chat.whatsapp.com/${linkgc}*`
+zero.sendMessage(from, yeh, text, { quoted: ftrol })
+break
+
+// Akhir Fitur!
+        
+default:
+if (budy.startsWith('>')) {
+console.log(color('[EVAL1]'), color(moment(zer.messageTimestamp * 1000).format('DD/MM/YY HH:mm:ss'), 'yellow'), color(`eval return`))
+try {
+let evaled = await eval(budy.slice(2))
+if (typeof evaled !== 'string') evaled = require('util').inspect(evaled)
+reply(`${evaled}`)
+} catch (err) {
+reply(`${err}`)
+}
+} else if (budy.startsWith('x')) {
+console.log(color('[EVAL2]'), color(moment(zer.messageTimestamp * 1000).format('DD/MM/YY HH:mm:ss'), 'yellow'), color(`eval identy`))
+try {
+return zero.sendMessage(from, JSON.stringify(eval(budy.slice(2)), null, '\t'), text, { quoted: ftrol })
+} catch (err) {
+e = String(err)
+reply(e)
+}
+}
+}
+} catch (e) {
+e = String(e)
+if (!e.includes("this.isZero") && !e.includes("jid")) {
+console.log('Error : %s', color(e, 'red'))
+}
+// console.log(e)
+}
+}
